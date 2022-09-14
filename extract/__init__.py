961f804a5f92a8d170672c9ce04584e18b0f22f5
Ali Gokce <ali.gokce@metu.edu.tr>
2022-09-14 14:45:52 +0300

    Added legendre extract job

--- extract/__init__.py
+++ extract/__init__.py
@@ -1,3 +1,4 @@
 from .shd import job as shd
 from .dpd_ratios import job as dpd_ratio
 from .pareto import job as pareto_params
+from .legendre import job as legendre_count
---
+++ extract/legendre.py
@@ -0,0 +1,81 @@
+from pathlib import Path
+
+import numpy as np
+from dataset import smir_datasets
+from features import legendre as lg
+from tqdm import tqdm
+from utils.save import save_lg_count
+
+from .shd import extract_shd
+
+
+def extract_legendre_count(
+    audio_file: str,
+    position: list or tuple,
+    # audio_folder: str = None,
+    # smir_folder: str = None,
+    # smir_room: str = None,
+    # samples: tuple = (0, 192000),
+    # fs: int = 48000,
+    # n_fft: int = 1024,
+    # n_channels: int = 32,
+    n_shd: int = 4,
+    # fl: float = 2608.0,
+    # fh: float = 5126.0,
+    # olap: int = 4,
+    # j_nu: int = 25,
+    # j_tau: int = 4,
+    # n_spcorr: int = 4,
+    # mic_name: str = 'em32',
+    smir_name: str = 'spargair',
+    npix: int = 192,
+    tol: float = 0.1,
+    save=False,
+    load_from_path=None,
+    **kwargs
+):
+    SMIRDataset = smir_datasets[smir_name]
+
+    ''' Extract
+    '''
+    if load_from_path:
+        p = Path(load_from_path)
+        assert p.isdir(), f"Given path is not a directory: {str(p)}"
+
+        load_path = p / SMIRDataset._pos_dir(position) / audio_file
+        if load_path.exists():
+            Anm = np.load(load_path.with_suffix('.npy'))
+        else:
+            print("File", load_path, "is not available, passing...")
+            return
+    else:
+        Anm = extract_shd(audio_file, position, n_shd=n_shd, smir_name=smir_name,
+                          **kwargs)
+
+    srf = lg.srf.srf_healpix(n_shd, Anm, npix)
+    dictionary = lg.generate_legendre_dict_healpix(n_shd, npix)
+    count = np.zeros(srf.shape[:2], dtype=int)
+    rent = np.zeros(srf.shape[:2], dtype=float)
+
+    for tind in tqdm(range(srf.shape[0]), desc="Extracting Legendre kernel counts..."):
+        for find in tqdm(range(srf.shape[1]), leave=False):
+            y = srf[tind, find]
+            result = lg.omp(dictionary, y, dtype=complex, tol=tol)
+            
+            count[tind, find] = len(result.coef.nonzero()[0])
+            rent[tind, find] = 1 - result.err[0]
+
+    ''' Saves
+    '''
+    if save:
+        save_path = Path.cwd() / SMIRDataset._pos_dir(position) / audio_file
+        save_path_count = save_path.with_name(save_path.stem + "_count" + ".npy")
+        save_lg_count(count, save_path_count, verbose=False)
+        save_path_rent = save_path.with_name(save_path.stem + "_rent" + ".npy")
+        save_lg_count(rent, save_path_rent, verbose=False)
+
+    return count, rent
+
+
+def job(*args, **kwargs):
+    extract_legendre_count(*args, **kwargs)
