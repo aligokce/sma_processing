815f2bf1a9b0a632f60af73ca84fa1e534774a92
Ali Gokce <ali.gokce@metu.edu.tr>
2022-09-21 14:20:54 +0300

    Refactored main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import itertools
-import sys
 from functools import partial
+from itertools import product
 from multiprocessing import Pool
+from pathlib import Path
 
 import hydra
 from omegaconf import DictConfig, OmegaConf
@@ -10,29 +10,45 @@ import extract
 from dataset import smir_datasets
 
 
+def get_files_and_positions(config):
+    audio_files = config.experiment.audio_files
+    if audio_files == 'all':
+        audio_files = [f.name for f in Path(
+            config.paths.audio_folder).glob("*.wav")]
+
+    positions = config.experiment.positions
+    if positions == 'all':
+        positions = smir_datasets[config.experiment.smir_name].generate_positions()
+    elif positions == 'perpendicular':
+        positions = smir_datasets[config.experiment.smir_name].generate_perpendicular_positions()
+
+    return audio_files, positions
+
+
+def get_job(config):
+    func = eval(config.experiment.job)
+    job = partial(
+        func,
+        **config.paths,
+        **config.experiment,
+        **config.params,
+    )
+
+    return job
+
+
 @hydra.main(config_path='configs', config_name='config', version_base=None)
 def main(config: DictConfig):
     print(OmegaConf.to_yaml(config))
 
-    audio_files = config.experiment.audio_files
-    positions = config.experiment.positions
-    if positions == 'all':
-        positions = smir_datasets[config.experiment.smir_name].generate_positions()
+    audio_files, positions = get_files_and_positions(config)
 
     with Pool(processes=config.misc.n_threads) as pool:
-        job = partial(
-            eval(config.experiment.job),
-            **config.paths,
-            **config.experiment,
-            **config.params,
+        pool.starmap(
+            get_job(config),
+            product(audio_files, positions)
         )
-        args_list = []
-        for file, pos in itertools.product(audio_files, positions):
-            args_list.append((file, pos))
-
-        pool.starmap(job, args_list)
 
 
 if __name__ == "__main__":
-    sys.argv.append('hydra.job.chdir=True')
     main()
